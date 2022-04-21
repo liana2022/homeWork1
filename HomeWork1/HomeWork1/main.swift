b5b7b3ceb5bce78a71f0c20a3163637a3343a701
Liana Asatryan <lianaasatryan@Lianas-MacBook-Pro.local>
2022-04-22 00:06:31 +0400

    Homework1 done

--- HomeWork1/HomeWork1/main.swift
+++ HomeWork1/HomeWork1/main.swift
@@ -9,3 +9,126 @@ import Foundation
 
 print("Hello, World!")
 
+//Homework 1
+
+var a1: Int = 100
+var b1: Int = 399
+var c1: Float = 3.55
+var d1: Double = -5.66
+var e1: Int = 1
+var f1: Int = 0
+var g1: Int = -1
+var k1: String = "String"
+var h1: String = "Hello"
+
+print("a1 = \(a1), b1 = \(b1), c1 = \(c1), d1 = \(d1), e1 = \(e1), f1 = \(f1), g1 = \(g1), k1 = \(k1), h1 = \(h1)")
+
+//Homework 2
+
+let a2: Int = 100
+let b2: Int = 399
+let c2: Float = 3.55
+let d2: Double = -5.66
+let e2: Int = 1
+let f2: Int = 0
+let g2: Int = -1
+let k2: String = "String"
+let h2: String = "Hello"
+
+print("a2 = \(a2), b2 = \(b2), c2 = \(c2), d2 = \(d2), e2 = \(e2), f2 = \(f2), g2 = \(g2), k2 = \(k2), h2 = \(h2)")
+
+
+//Homework 3
+
+let a3: Int = 10
+let b3: Int = 399
+let result3: Int = a3 + b3
+
+print("result3 = \(a3 + b3)")
+
+
+//Homework 4
+
+let a4: Int = 13
+let b4: Int = 459
+let result4: Int = a4 - b4
+
+print("result4 = \(a4 - b4)")
+
+//Homework 5
+
+let a5: Int = 13
+let b5: Int = 459
+let result5: Int = a5 * b5
+
+print("result5 = \(a5 * b5)")
+
+// Homework 6
+
+let a6: Int = 400
+let b6: Int = 3
+let result6: Int = a6 / b6
+
+print("result6 = \(a6 / b6)")
+
+//Homework 7
+
+let a7: Int = 456
+let b7: Int = 3
+let result7: Int = a7 % b7
+
+print("result7 = \(a7 % b7)")
+
+//Homework 8
+
+var a8 = 100
+let b8 = 300
+a8 += b8
+ print("a8 = \(a8), b8 = \(b8)")
+
+//Homework 9
+
+var a9 = 100
+let b9 = 300
+a9 *= b9
+
+print("a9 = \(a9), b9 = \(b9)")
+
+
+//Homework 10
+
+var a10 = 100
+let b10 = 300
+a10 -= b10
+
+print("a10 = \(a10), b10 = \(b10)")
+
+//Homework 11
+
+var a11 = 300
+let b11 = 100
+a11 /= b11
+
+print("a11 = \(a11), b11 = \(b11)")
+
+
+//Homework 12
+
+var a12 = 300
+let b12 = 100
+a12 = b12
+
+print("a12 = \(a12)")
+
+//Homework 13
+
+let a13 = 13
+let b13 = 459
+let isEqual = a13 == b13
+let isLess = a13 < b13
+let isGreater = a13 > b13
+let isNotEqual = a13 != b13
+
+
+print("isEqual = \(isEqual), isLess = \(a13 < b13), isGreater = \(a13 > b13), isNotequal = \(a13 != b13)")
+
