489fc29f30ba5ba4e3ec03bb4a0a53619bd5722f
Liana Asatryan <lianaasatryan@Lianas-MacBook-Pro.local>
2022-04-25 23:12:31 +0400

    Homework2 done

--- HomeWork1/HomeWork1/main.swift
+++ HomeWork1/HomeWork1/main.swift
@@ -4,131 +4,201 @@
 //
 //  Created by Liana Asatryan on 21.04.22.
 //
+//
+//import Foundation
+//
+//print("Hello, World!")
+//
+////Homework 1
+//
+//var a1: Int = 100
+//var b1: Int = 399
+//var c1: Float = 3.55
+//var d1: Double = -5.66
+//var e1: Int = 1
+//var f1: Int = 0
+//var g1: Int = -1
+//var k1: String = "String"
+//var h1: String = "Hello"
+//
+//print("a1 = \(a1), b1 = \(b1), c1 = \(c1), d1 = \(d1), e1 = \(e1), f1 = \(f1), g1 = \(g1), k1 = \(k1), h1 = \(h1)")
+//
+////Homework 2
+//
+//let a2: Int = 100
+//let b2: Int = 399
+//let c2: Float = 3.55
+//let d2: Double = -5.66
+//let e2: Int = 1
+//let f2: Int = 0
+//let g2: Int = -1
+//let k2: String = "String"
+//let h2: String = "Hello"
+//
+//print("a2 = \(a2), b2 = \(b2), c2 = \(c2), d2 = \(d2), e2 = \(e2), f2 = \(f2), g2 = \(g2), k2 = \(k2), h2 = \(h2)")
+//
+//
+////Homework 3
+//
+//let a3: Int = 10
+//let b3: Int = 399
+//let result3: Int = a3 + b3
+//
+//print("result3 = \(a3 + b3)")
+//
+//
+////Homework 4
+//
+//let a4: Int = 13
+//let b4: Int = 459
+//let result4: Int = a4 - b4
+//
+//print("result4 = \(a4 - b4)")
+//
+////Homework 5
+//
+//let a5: Int = 13
+//let b5: Int = 459
+//let result5: Int = a5 * b5
+//
+//print("result5 = \(a5 * b5)")
+//
+//// Homework 6
+//
+//let a6: Int = 400
+//let b6: Int = 3
+//let result6: Int = a6 / b6
+//
+//print("result6 = \(a6 / b6)")
+//
+////Homework 7
+//
+//let a7: Int = 456
+//let b7: Int = 3
+//let result7: Int = a7 % b7
+//
+//print("result7 = \(a7 % b7)")
+//
+////Homework 8
+//
+//var a8 = 100
+//let b8 = 300
+//a8 += b8
+// print("a8 = \(a8), b8 = \(b8)")
+//
+////Homework 9
+//
+//var a9 = 100
+//let b9 = 300
+//a9 *= b9
+//
+//print("a9 = \(a9), b9 = \(b9)")
+//
+//
+////Homework 10
+//
+//var a10 = 100
+//let b10 = 300
+//a10 -= b10
+//
+//print("a10 = \(a10), b10 = \(b10)")
+//
+////Homework 11
+//
+//var a11 = 300
+//let b11 = 100
+//a11 /= b11
+//
+//print("a11 = \(a11), b11 = \(b11)")
+//
+//
+////Homework 12
+//
+//var a12 = 300
+//let b12 = 100
+//a12 = b12
+//
+//print("a12 = \(a12)")
+//
+////Homework 13
+//
+//let a13 = 13
+//let b13 = 459
+//let isEqual = a13 == b13
+//let isLess = a13 < b13
+//let isGreater = a13 > b13
+//let isNotEqual = a13 != b13
+//
+//
+//print("isEqual = \(isEqual), isLess = \(a13 < b13), isGreater = \(a13 > b13), isNotequal = \(a13 != b13)")
 
-import Foundation
+//unary
+// -a +a !a a!
 
-print("Hello, World!")
-
-//Homework 1
-
-var a1: Int = 100
-var b1: Int = 399
-var c1: Float = 3.55
-var d1: Double = -5.66
-var e1: Int = 1
-var f1: Int = 0
-var g1: Int = -1
-var k1: String = "String"
-var h1: String = "Hello"
-
-print("a1 = \(a1), b1 = \(b1), c1 = \(c1), d1 = \(d1), e1 = \(e1), f1 = \(f1), g1 = \(g1), k1 = \(k1), h1 = \(h1)")
-
-//Homework 2
-
-let a2: Int = 100
-let b2: Int = 399
-let c2: Float = 3.55
-let d2: Double = -5.66
-let e2: Int = 1
-let f2: Int = 0
-let g2: Int = -1
-let k2: String = "String"
-let h2: String = "Hello"
-
-print("a2 = \(a2), b2 = \(b2), c2 = \(c2), d2 = \(d2), e2 = \(e2), f2 = \(f2), g2 = \(g2), k2 = \(k2), h2 = \(h2)")
+//var a: Int = -5
+//
+// a = +a
+//print("a = \(a)")
 
 
-//Homework 3
+//var a: Bool = true
+//print("a = \(a) !a =\(!a)")
 
-let a3: Int = 10
-let b3: Int = 399
-let result3: Int = a3 + b3
+//ternary
 
-print("result3 = \(a3 + b3)")
+//  <Bool> ? 10 / a : 20 \ b
+//
+//// isTrue == True a = 10 || isTrue == false a = 20
+//let isTrue: Bool = true
+//
+//var a: Int = (isTrue ? 10 : 20)
 
+// carMassKg -> 1000
+//hasPassenger = / true / false
+// isChildPassenger -> Bool
+// passengerMassKg = 60
+// childpassenger = 30
 
-//Homework 4
+// allCarMassKg = carMassKg + passengerMassKg
+// allCarMassKg = carMassKg
 
-let a4: Int = 13
-let b4: Int = 459
-let result4: Int = a4 - b4
-
-print("result4 = \(a4 - b4)")
-
-//Homework 5
-
-let a5: Int = 13
-let b5: Int = 459
-let result5: Int = a5 * b5
-
-print("result5 = \(a5 * b5)")
-
-// Homework 6
-
-let a6: Int = 400
-let b6: Int = 3
-let result6: Int = a6 / b6
-
-print("result6 = \(a6 / b6)")
-
-//Homework 7
-
-let a7: Int = 456
-let b7: Int = 3
-let result7: Int = a7 % b7
-
-print("result7 = \(a7 % b7)")
-
-//Homework 8
-
-var a8 = 100
-let b8 = 300
-a8 += b8
- print("a8 = \(a8), b8 = \(b8)")
-
-//Homework 9
-
-var a9 = 100
-let b9 = 300
-a9 *= b9
-
-print("a9 = \(a9), b9 = \(b9)")
+//var carMassKg = 1000
+//let passengerMasskg = 60
+//let passengerFull = true
+//var allCarMasskg = (passengerFull ? carMassKg + passengerMasskg : carMassKg)
+//
+//print(allCarMasskg)
+//
+//var carMassKg = 1000
+//let passengerFul = true
+//var passengerMassKg = 60
+//var childpassengerYes = false
+//var childpassenger = 30
+//var childpassenger = true
+//var allcarMasskg = (passengerFul ? ( childpassengerYes ? carMassKg + childpassenger : carMassKg ) : carMassKg )
+//
+//print(allcarMasskg)
 
 
-//Homework 10
-
-var a10 = 100
-let b10 = 300
-a10 -= b10
-
-print("a10 = \(a10), b10 = \(b10)")
-
-//Homework 11
-
-var a11 = 300
-let b11 = 100
-a11 /= b11
-
-print("a11 = \(a11), b11 = \(b11)")
-
-
-//Homework 12
-
-var a12 = 300
-let b12 = 100
-a12 = b12
-
-print("a12 = \(a12)")
-
-//Homework 13
-
-let a13 = 13
-let b13 = 459
-let isEqual = a13 == b13
-let isLess = a13 < b13
-let isGreater = a13 > b13
-let isNotEqual = a13 != b13
-
-
-print("isEqual = \(isEqual), isLess = \(a13 < b13), isGreater = \(a13 > b13), isNotequal = \(a13 != b13)")
-
+//
+//let str: String = "Hello"
+//
+//let strEmpty: String = "a"
+//let strEmpty1: String = Striong()
+//
+//
+////print("strEmpty")
+//
+//var str: String = "Hello"
+//let space: String = " "
+//let str2: String = "GITC"
+//
+//str += space // "Hello"
+//str += str2 // "Hello GITC"
+//
+//let result: String = (str + space + str2)
+//
+//str.append(space)
+//str.append(str2)
+//
+//print("result = \(result)")
---
+++ HomeWork2/HomeWork2/main.swift
@@ -0,0 +1,139 @@
+//
+//  main.swift
+//  HomeWork2
+//
+//  Created by Liana Asatryan on 23.04.22.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+//Homework 1
+
+var a1 = 5
+var b1 = 145
+var c1 = -1234.33
+var d1 = false
+a1 = -a1
+b1 = -b1
+c1 = -c1
+d1 = !d1
+
+print("a1 = \(a1), b1 = \(b1), c1 = \(c1), d1 = \(d1)")
+
+//Homework 2
+
+let isTen2 = false
+let a2 = isTen2 ? 10 : 45
+print("a2 = \(a2)")
+
+//Homework 3
+
+let isGirl3 = false
+let person3 = isGirl3 ? "is Girl" : "is Boy"
+print("person3 = \(person3)")
+
+//Homework 4
+
+let a4 = 35.7
+let b4 = 47.67
+let max4 = a4 > b4 ? a4 : b4
+print("max4 = \(max4)")
+
+
+//Homework 5
+
+let a5 = 35.7
+let b5 = 47.67
+let min5 = a5 < b5 ? a5 : b5
+print("min5 = \(min5)")
+
+//Homework 6
+
+let a6 = 3
+let b6 = 1096
+let c6 = -36
+let max6 = a6 > b6 ? ( a6 > c6 ? a6 : c6) : (b6 > c6 ? b6 : c6)
+print(max6)
+
+//Homework 7
+
+let a7 = 3
+let b7 = 1096
+let c7 = -36
+let min7 = a7 < b7 ? (a7 <  c7 ? a7 : c7) : (b7 < c7 ? b7 : c7)
+print(min7)
+
+//Homework 8
+
+let str8 = ""
+print(str8.isEmpty)
+
+//Homework 9
+
+let str9 = "Hello"
+print(str9.count)
+
+//Homework 11
+
+var str11 = "Hello"
+str11.append("GITC")
+print(str11)
+
+//Homework 12
+
+let str12 = "Hello"
+print(str12[str12.startIndex])
+
+//HomeWork 13
+let str13 = "Hello"
+let index13 = str13.index(before: str13.endIndex)
+print(str13[index13])
+
+//Homework 14
+
+let str14 = "Hello"
+let index14 = str14.index(str14 .startIndex, offsetBy: str14.count/2)
+print("str14 = \(str14[index14])")
+
+//Homework 15
+
+var str15 = "Hello"
+str15.insert(")", at: str15.startIndex)
+print(str15)
+
+// Homework 16
+
+var str16 = "Hello"
+let index16 = str16.index(after: str16.startIndex)
+str16.insert("-", at: index16)
+print(str16)
+
+//Homework 17
+
+var str17 = "hello"
+let index17 = str17.index(after: str17.startIndex)
+str17.insert(contentsOf: "—Error—", at: index17)
+print(str17)
+
+//Homework 18
+
+var str18 = "Hello"
+str18.remove(at: str18.startIndex)
+print(str18)
+
+//Homework 19
+
+var str19 = "hello"
+let index19 = str19.index(before: str19.endIndex)
+str19.remove(at: index19)
+print(str19)
+
+//Homework 20
+
+var str20 = "Hello"
+let index20 = str20.index(str20.startIndex, offsetBy: str20.count/2)
+str20.remove(at: index20)
+print(str20)
+
